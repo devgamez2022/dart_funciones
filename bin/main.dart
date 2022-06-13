e05a1a8e039db5595ae4546135fe36f77af05ac2
devgamez2022 <winmanuel09@gmail.com>
2022-06-13 10:12:11 -0600

    funciones en dart

---
+++ bin/main.dart
@@ -0,0 +1,54 @@
+void main(List<String> args) {
+  String nombre = "Manuel de Jesús Gámez López";
+
+  var operacion1 = operacion(5, 3, suma);
+  print("La suma es: $operacion1");
+
+  var operacion2 = operacion(5, 3, resta);
+  print("La resta es: $operacion2");
+
+  /*
+  int a = 10;
+  int b = 5;
+  int c = suma(a, b);
+  print("La suma de $a y $b es: $c");
+  */
+
+  mensaje1(nombre);
+}
+
+int operacion(int a, int b, Function func) {
+  return func(a, b);
+}
+
+int suma(int a, int b) {
+  return a + b;
+}
+
+int resta(int a, int b) {
+  return a - b;
+}
+
+void mensaje() {
+  var textoSaludo = "";
+  textoSaludo = "Bienvenidos a Dart.\n";
+  textoSaludo +=
+      "Soy el profesor que les estare guiando por este aprendizaje.\n";
+  textoSaludo += "Espero nos podamos llevar bien.\n";
+  textoSaludo +=
+      "Y enseñarles mucho de este genial lenguaje de programacion.\n";
+  textoSaludo += "Es para mi un privilegio apoyar en este proceso.";
+  print(textoSaludo);
+}
+
+void mensaje1(String nombre) {
+  var textoSaludo = "";
+  textoSaludo = "Bienvenidos a Dart $nombre\n";
+  textoSaludo +=
+      "Soy el profesor que les estare guiando por este aprendizaje.\n";
+  textoSaludo += "Espero nos podamos llevar bien.\n";
+  textoSaludo +=
+      "Y enseñarles mucho de este genial lenguaje de programacion.\n";
+  textoSaludo += "Es para mi un privilegio apoyar en este proceso.";
+  print(textoSaludo);
+}
---
+++ test/dart_funcion1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_funcion1/dart_funcion1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
